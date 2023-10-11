cf55cb7861a740372a3a5cebda98de433ac709ec
dbergs72 <dbergs72@gmail.com>
2023-10-11 12:49:43 -0400

    up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
 import Labs from "./Labs";
 import HelloWorld from "./Labs/a3/HelloWorld";
 import Kanbas from "./Kanbas";
-
+import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
 function App() {
-  return (
-    <div>
-        <HelloWorld />
-        <Labs />
-        <Kanbas />
-    </div>
-  );
+    return (
+        <HashRouter>
+            <div>
+                <Routes>
+                    <Route path="/"         element={<Navigate to ="/Labs"/>}/>
+                    <Route path="/hello"    element={<HelloWorld/>}/>
+                    <Route path="/Labs/*"   element={<Labs/>}/>
+                    <Route path="/Kanbas/*" element={<Kanbas/>}/>
+                </Routes>
+            </div>
+        </HashRouter>
+    );
 }
-
 export default App;
--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,11 +1,15 @@
+import {Link} from "react-router-dom";
 import Assignment3 from "./a3";
-
 function Labs() {
     return(
-        <div className="container">
-            <Assignment3 />
+        <div>
+            <Link to="/hello">Hello</Link>
+            <Link to="/Labs/a3">A3</Link>
+            <Link to="/Kanbas">Kanbas</Link>
+            <Assignment3/>
         </div>
-    )
+    );
 }
+
 
 export default Labs;
