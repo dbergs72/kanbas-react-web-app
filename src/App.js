9a87f97f6ef66707f77153ff2052f80e794b456f
dbergs72 <dbergs72@gmail.com>
2023-10-12 21:06:59 -0400

    Import starter code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
+import Kanbas from "./Kanbas";
 import Labs from "./Labs";
 import HelloWorld from "./Labs/a3/HelloWorld";
-import Kanbas from "./Kanbas";
-import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
+import logo from "./logo.svg";
+// import "./App.css";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
+
 function App() {
-    return (
-        <HashRouter>
-            <div>
-                <Routes>
-                    <Route path="/"         element={<Navigate to ="/Labs"/>}/>
-                    <Route path="/hello"    element={<HelloWorld/>}/>
-                    <Route path="/Labs/*"   element={<Labs/>}/>
-                    <Route path="/Kanbas/*" element={<Kanbas/>}/>
-                </Routes>
-            </div>
-        </HashRouter>
-    );
+  const screen = "labs";
+  return (
+    <HashRouter>
+      <div>
+        <div className="list-group">
+          <Link className="list-group-item" to="/Labs">
+            Labs
+          </Link>
+          <Link className="list-group-item" to="/hello">
+            Hello
+          </Link>
+          <Link className="list-group-item" to="/kanbas">
+            Kanbas
+          </Link>
+        </div>
+        <Routes>
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+          <Route path="/hello" element={<HelloWorld />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
 }
+
 export default App;
