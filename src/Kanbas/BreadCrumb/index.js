4c5212a13ea8a77c2b73d665b5eb864e53df8652
dbergs72 <dbergs72@gmail.com>
2023-10-18 15:51:04 -0400

    3.4 fully polished

--- src/Kanbas/BreadCrumb/index.js
+++ src/Kanbas/BreadCrumb/index.js
@@ -6,15 +6,13 @@ import db from "../Database";
 function BreadCrumb() {
     const { courseId } = useParams();
     const { pathname } = useLocation();
-    let course = db.courses.find((course) => course._id === courseId);
-    if (course === undefined) {
-        course = {name: "Courses"};
-    }
+    const course = db.courses.find((course) => course._id === courseId);
+    console.log(course);
     return (
         <div className={"d-flex flex-row"}>
             <FaGripLines  size={30} className={"wd-kanbas-red"} style={{ paddingTop: 10, paddingLeft: 10}}/>
             <ol className="breadcrumb pt-2 ps-4">
-                <li className="breadcrumb-item wd-kanbas-red">{course.name}</li>
+                {course !== undefined && <li className="breadcrumb-item wd-kanbas-red">{course.name}</li>}
                 <li className="breadcrumb-item active" aria-current="page">{pathname.substring(pathname.lastIndexOf("/") + 1)}</li>
             </ol>
         </div>
