d238b7996367ccf26a49b40b0d8e3802b746f42b
dbergs72 <dbergs72@gmail.com>
2023-11-09 09:30:13 -0500

    3.3 done

--- src/Labs/a5/index.js
+++ src/Labs/a5/index.js
@@ -1,11 +1,13 @@
 import EncodingParametersInURLs from "./EncodingParametersInURLs.js";
 import WorkingWithObjects from "./WorkingWithObjects.js";
+import WorkingWithArrays from "./WorkingWithArrays";
 
 function Assignment5() {
   return (
     <div>
       <h1>Assignment 5</h1>
       <div className="list-group">
+        <WorkingWithArrays />
         <WorkingWithObjects />
         <EncodingParametersInURLs />
         <a href="http://localhost:4000/a5/add/5/6" className="list-group-item">
